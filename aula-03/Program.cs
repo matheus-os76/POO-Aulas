05fcdf34c3512a3af1675edc82d5784edcff3850
José Romualdo <joseromualdo@outlook.com>
2025-02-19 14:25:52 +0000

    feat: adicionar métodos para gestão de volume

--- aula-03/Program.cs
+++ aula-03/Program.cs
@@ -5,9 +5,20 @@ Console.WriteLine("Hello, World!");
 
 int idade = 32;
 
-Televisao tvSala = new Televisao(55);
+Televisao tvSala = new Televisao(22f);
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
 
 Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+
+tvSala.AumentarVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
+
+
 
 
 
--- aula-03/Televisao.cs
+++ aula-03/Televisao.cs
@@ -11,8 +11,20 @@ class Televisao
     //visibilidades e recebendo parametros, se necessário.
     public Televisao(float tamanho)
     {
+        if (tamanho < TAMANHO_MINIMO || tamanho > TAMANHO_MAXIMO)
+        {
+            throw new ArgumentOutOfRangeException($"O tamanho({tamanho}) não é suportado!");
+        }
         Tamanho = tamanho;
-    }    
+        Volume = VOLUME_PADRAO;
+    }
+
+    //Optamos pela utilização da constante para tornar o código mais legível.
+    private const float TAMANHO_MINIMO = 22;
+    private const float TAMANHO_MAXIMO = 80;
+    private int VOLUME_MAXIMO = 12;
+    private int VOLUME_MINIMO = 0;
+    private int VOLUME_PADRAO = 10;
 
 
 
@@ -20,15 +32,39 @@ class Televisao
     //leitura do valor atual da propriedade
     //Set: permite que seja atibuído um 
     //valor para a propriedade
-    
+
     //classes, propriedades e métodos possuem modificadores de acesso
     //public: visiveis a todo o projeto
     //internal: visiveis somente no namespace - padrão
     //protected: visiveis somente na classe e nas classes que herdam
     //private: visiveis somente na classe que foram criados
-    public float Tamanho { get; private set; }
+    public float Tamanho { get; }
     public int Resolucao { get; set; }
-    public int Volume { get; set; }
+    public int Volume { get; private set; }
     public int Canal { get; set; }
     public bool Estado { get; set; }
+
+    public void AumentarVolume()
+    {
+        if (Volume < VOLUME_MAXIMO)
+        {
+            Volume++;
+        }
+        else
+        {
+            Console.WriteLine("A TV já está no volume máximo permitido");
+        }
+    }
+
+    public void DiminuirVolume()
+    {
+        if (Volume > VOLUME_MINIMO)
+        {
+            Volume--;
+        }
+        else
+        {
+            Console.WriteLine("A TV já está no volume mínimo permitido");
+        }
+    }
 }
