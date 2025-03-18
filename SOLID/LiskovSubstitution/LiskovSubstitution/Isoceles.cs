995cf06b1a48eca2697a4a74d6d1cec6732faf68
matheus-os76 <matheusosmedio1@gmail.com>
2025-03-18 19:09:19 -0300

    Criação do LiskovSubstitution

---
+++ SOLID/LiskovSubstitution/LiskovSubstitution/Isoceles.cs
@@ -0,0 +1,10 @@
+namespace geometria;
+
+public class Isoceles : Triangulo
+{
+    public Isoceles()
+    {
+        tipo = "isoceles";
+    }
+    public string tipo { get; }
+}
---
+++ SOLID/LiskovSubstitution/LiskovSubstitution/Triangulo.cs
@@ -0,0 +1,9 @@
+namespace geometria;
+
+public class Triangulo
+{
+    public float hipotenusa { get; set; }
+    public float cateto_A { get; set; }
+    public float cateto_B { get; set; }
+
+}
