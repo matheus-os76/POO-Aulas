d26af1752bff799027ae3efada6667b569501d6a
matheus-os76 <matheusosmedio1@gmail.com>
2025-05-21 11:06:40 -0300

    Classe Telefone feita - Prova

---
+++ Prova-POO/Gerenciador_Mensagens/Program.cs
@@ -0,0 +1,5 @@
+﻿using Gerenciador_Mensagens;
+
+Telefone telefone = new Telefone("2098190 0792");
+
+Console.WriteLine(telefone.Estado);
---
+++ Prova-POO/Gerenciador_Mensagens/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_Mensagens
+{
+    internal class Usuario
+    {
+        public string Nome { get; set; }
+
+        public Telefone Telefone { get; set; }
+    }
+}
